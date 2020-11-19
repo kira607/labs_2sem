79e2c94b541d8f1df13738dfa12017177568af72
kira607 <kirill.lesckin@gmail.com>
2020-11-19 18:37:40 +0300

    Add route db

--- CR/src/destination.h
+++ CR/src/destination.h
@@ -9,13 +9,13 @@
 
 enum class Destination
 {
-    Omsk,
-    Kaliningrad,
-    StPetersburg,
-    Novgorod,
-    Sochi,
-    Murmansk,
-    NONE
+    Omsk = 1,
+    Kaliningrad = 2,
+    StPetersburg = 3,
+    Novgorod = 4,
+    Sochi = 5,
+    Murmansk = 6,
+    NONE = 0
 };
 
 std::string str(Destination destination);
--- CR/src/main.cpp
+++ CR/src/main.cpp
@@ -1,13 +1,21 @@
 #include "truck.h"
 #include "driver.h"
+#include "route.h"
 
 int main()
 {
     TruckDataBase truck_db = TruckDataBase("../dbs/truckdb.csv");
-    truck_db.list.Print(10);
+    truck_db.list.PrintAll();
+
+    std::cout << "=============\n";
 
     DriverDataBase driver_db = DriverDataBase("../dbs/driverdb.csv");
     driver_db.list.PrintAll();
+
+    std::cout << "=============\n";
+
+    RouteDataBase route_db = RouteDataBase();
+    route_db.list.PrintAll();
 
     return 0;
  }
