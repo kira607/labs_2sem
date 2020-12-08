f16a05dc9cb9f909662ec393bcf0229c842d6394
kira607 <kirill.lesckin@gmail.com>
2020-12-08 19:48:55 +0300

    additions

--- CR/src/common/request.h
+++ CR/src/common/request.h
@@ -1,12 +1,6 @@
 #ifndef COURSEWORK_REQUEST_H
 #define COURSEWORK_REQUEST_H
 
-#include <fstream>
-#include <string>
-
-#define CSV_IO_NO_THREAD
-#include "../../fast-cpp-csv-parser/csv.h"
-
 #include "destination.h"
 #include "truck_brand.h"
 #include "date.h"
@@ -20,12 +14,6 @@ struct Request
     Route *target_route{};
     Destination destination = Destination::NONE;
     TruckBrand truck_brand = TruckBrand::NONE;
-};
-
-struct RequestLoader
-{
-    Request request;
-    Request Load(const std::string &file_name);
 };
 
 #endif //COURSEWORK_REQUEST_H
